6a0c6a12011e31f10d82325784238a3df3d15335
Vinícius Brait <121905548+devBrait@users.noreply.github.com>
2024-02-28 23:46:53 +0000

    Enviando último arquivo da aula 02

---
+++ aula02/areaCirculo.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+  int raio;
+  double areaCirculo;
+
+  printf("Informe o valor do raio: \n");
+  scanf("%d", &raio);
+
+  areaCirculo = M_PI*(raio*raio);
+  printf("A área do círculo é igual a %.2lf", areaCirculo);
+}
